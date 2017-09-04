132914682a96cbaeb612e96788d57ad82e3bfc6a
milosz <milosz@PC>
2017-09-04 15:35:48 +0200

    animations_assigned

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+	//public Material material;
+	public Texture onDay;
+	public Texture onEvening;
+	public Texture onNight;
+
+	private MeshRenderer renderer;
+	private Texture[] textures = new Texture[3];
+	private int index = 0;
+
+	void Start () {
+		textures [0] = onDay;
+		textures [1] = onEvening;
+		textures [2] = onNight;
+		//material.SetTexture ("sky", onDay);
+		renderer = this.GetComponent<MeshRenderer>();
+	}
+
+	void Update () {
+
+		if (Input.GetKeyUp ("b")) {
+			Debug.Log("kazam!");
+		/*	index++;
+			if (index > 2) {
+				index = 0;
+			}
+			*/
+			renderer.material.SetTexture ("sky", onEvening);
+		}
+	}
+}
--- Assets/Scripts/CreatureController.cs
+++ Assets/Scripts/CreatureController.cs
@@ -4,13 +4,67 @@ using UnityEngine;
 
 public class CreatureController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	private Animator animator;
+	private int level = 0;
+	private int overallSatisfaction = 0;
+	private int satisfaction = 0;
+	private int weight = 0;
+
+	void Awake(){
+
+		animator = GetComponent<Animator> ();
+
+		//level = setLevel();
+		//overallSatisfaction = setOverallSatisfaction();
+		//satisfaction = setSatisfaction ();
+		//weight = setWeight ();
+
 	}
+
+	public void setLevel(int newValue){
 	
-	// Update is called once per frame
-	void Update () {
-		
+		level = newValue;	
 	}
+
+	public void setOverallSatisfaction(int newValue){
+
+		overallSatisfaction = newValue;	
+	}
+
+	public void setSatisfaction(int newValue){
+
+		satisfaction = newValue;	
+	}
+
+	public void setWeight(int newValue){
+
+		weight = newValue;	
+	}
+
+	void Update () {
+
+		if (Input.GetKeyUp ("q")) {
+			animator.SetTrigger ("idle");
+			Debug.Log ("creature got back to normal!");
+		}
+		if (Input.GetKeyUp ("d")) {
+			animator.SetTrigger ("dead");
+			Debug.Log ("creature died!");
+		}
+		if (Input.GetKeyUp ("e")) {
+			animator.SetTrigger ("evolve");
+			Debug.Log ("creature has evolved!");
+		}
+		if (Input.GetKeyUp ("a")) {
+			animator.SetTrigger ("sleep");
+			Debug.Log ("creature is fast asleep!");
+		}
+		if (Input.GetKeyUp ("s")) {
+			animator.SetTrigger ("sad");
+			Debug.Log ("creature is sad!");
+		}
+
+
+	}
+
 }
