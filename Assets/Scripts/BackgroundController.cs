eb11c528f0cd98e8e2d9f2a2fcb29c6759f1e17e
milosz <milosz@PC>
2017-09-04 16:58:37 +0200

    changable background

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -8,29 +8,28 @@ public class BackgroundController : MonoBehaviour {
 	public Texture onDay;
 	public Texture onEvening;
 	public Texture onNight;
+	public Renderer renderer;
 
-	private MeshRenderer renderer;
-	private Texture[] textures = new Texture[3];
+	private Texture[] textures;
 	private int index = 0;
 
 	void Start () {
+		textures = new Texture[3];
 		textures [0] = onDay;
 		textures [1] = onEvening;
 		textures [2] = onNight;
-		//material.SetTexture ("sky", onDay);
-		renderer = this.GetComponent<MeshRenderer>();
+		//renderer = this.GetComponent<MeshRenderer>();
 	}
 
 	void Update () {
 
 		if (Input.GetKeyUp ("b")) {
 			Debug.Log("kazam!");
-		/*	index++;
+			index++;
 			if (index > 2) {
 				index = 0;
 			}
-			*/
-			renderer.material.SetTexture ("sky", onEvening);
+			renderer.material.SetTexture ("_MainTex", textures[index]);
 		}
 	}
 }
